79838a668f051da2c3d6d8ee854d80bf51b84971
nishioka-m <nishioka.m@world-wing.com>
2024-12-04 22:32:37 +0900

    fmt適用

--- gcs.tf
+++ gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "tfcmt-cicd-test" {
-  name          = "tfcmt-cicd-test"
-  location      = "asia-northeast1"
+  name     = "tfcmt-cicd-test"
+  location = "asia-northeast1"
 }
 #test
