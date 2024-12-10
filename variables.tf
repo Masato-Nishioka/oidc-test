500e95e173ca6efbd1a406c4052024d0b9cbb2c4
Masato-Nishioka <nishioka.masa0124@gmail.com>
2024-12-10 16:37:09 +0900

    variables.tfとmain.tfを更新し、locationのバリデーションを追加

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "location" {
   description = "Cloud Storage バケットのリージョン"
   type        = string
-  default     = "asia-northeast1"  # デフォルトを東京リージョンに設定
+  default     = "asia-northeast100"  # デフォルトを東京リージョンに設定
 
   validation {
     condition     = contains(["asia-northeast1", "asia-northeast2", "asia-northeast3"], var.location)
